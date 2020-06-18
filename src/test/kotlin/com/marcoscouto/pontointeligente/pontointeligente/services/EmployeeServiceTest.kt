b39dd8b7be12a0f2631146d0618cecce24217fa8
Marcos Couto <marcos_otuoc@hotmail.com>
2020-06-18 16:24:37 -0300

    Modify Employee test

--- src/test/kotlin/com/marcoscouto/pontointeligente/pontointeligente/services/EmployeeServiceTest.kt
+++ src/test/kotlin/com/marcoscouto/pontointeligente/pontointeligente/services/EmployeeServiceTest.kt
@@ -3,16 +3,17 @@ package com.marcoscouto.pontointeligente.pontointeligente.services
 import com.marcoscouto.pontointeligente.pontointeligente.documents.Employee
 import com.marcoscouto.pontointeligente.pontointeligente.enums.ProfileEnum
 import com.marcoscouto.pontointeligente.pontointeligente.repositories.EmployeeRepository
+import com.marcoscouto.pontointeligente.pontointeligente.utils.PasswordUtils
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.mockito.BDDMockito
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import java.lang.Exception
 import java.util.*
 
 @SpringBootTest
@@ -31,8 +32,8 @@ class EmployeeServiceTest() {
 
     @BeforeAll
     @Throws(Exception::class)
-    fun setUp(){
-        BDDMockito.given(repository?.save(employee())).willReturn(employee())
+    fun setUp() {
+        BDDMockito.given(repository?.save(Mockito.any(Employee::class.java))).willReturn(employee())
         BDDMockito.given(repository?.findByCpf(CPF)).willReturn(employee())
         BDDMockito.given(repository?.findByEmail(EMAIL)).willReturn(employee())
         BDDMockito.given(repository?.findById(ID)).willReturn(Optional.of(employee()))
@@ -50,8 +51,9 @@ class EmployeeServiceTest() {
     @Test
     fun shouldFindById() = Assertions.assertNotNull(service?.findById(employee()))
 
-    fun employee() = Employee("Employee", EMAIL, "123", CPF, ProfileEnum.ROLE_USER,
-                    "1", 1.0, 1f, 1f, ID)
+    fun employee() = Employee("Employee", EMAIL, PasswordUtils().generateBCrypt("123"),
+            CPF, ProfileEnum.ROLE_USER, "1", 1.0,
+            1f, 1f, ID)
 
 
 }
