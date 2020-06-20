33963e3834e57be1e15d9229efdee26d1fc1d6dc
Marcos Couto <marcos_otuoc@hotmail.com>
2020-06-20 13:11:06 -0300

    Entry DTO

---
+++ src/main/kotlin/com/marcoscouto/pontointeligente/pontointeligente/dtos/EntryDTO.kt
@@ -0,0 +1,18 @@
+package com.marcoscouto.pontointeligente.pontointeligente.dtos
+
+import javax.validation.constraints.NotEmpty
+
+data class EntryDTO(
+
+        @get:NotEmpty(message = "Data não pode ser vazia")
+        val data: String? = null,
+
+        @get:NotEmpty(message = "Tipo não pode ser vazio")
+        val type: String? = null,
+
+        val description: String? = null,
+        val localization: String? = null,
+        val employeeId: String? = null,
+        val id: String? = null
+
+)
